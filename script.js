ce3ef60d4655e57673d52fd1ee48827c1e03bf8b
unknown <nermen.morgan99@gmail.com>
2023-10-16 15:52:12 +0200

    initial commit

--- script.js
+++ script.js
@@ -69,7 +69,7 @@ function login() {
 // CheckBox Function
 function goFurther(){
   if (document.getElementById("chkAgree").checked == true) {
-    document.getElementById('btnSubmit').style = 'background: linear-gradient(to right, #FA4B37, #DF2771);';
+    document.getElementById('btnSubmit').style = 'background: linear-gradient(to right, #949be3, #030748);';
   }
   else{
     document.getElementById('btnSubmit').style = 'background: lightgray;';
@@ -132,26 +132,27 @@ function display(n) {
   img2.style = 'display: none;';
   img3.style = 'display: none;';
   img4.style = 'display: none;';
-  s1.style = 'background: #DF2771; color: #FFF;';
-  s2.style = 'background: #DF2771; color: #FFF;';
-  s3.style = 'background: #DF2771; color: #FFF;';
-  s4.style = 'background: #DF2771; color: #FFF;';
+
+  s1.style = 'background: #030748; color: #FFF;';
+  s2.style = 'background: #030748; color: #FFF;';
+  s3.style = 'background: #030748; color: #FFF;';
+  s4.style = 'background: #030748; color: #FFF;';
 
   if(n==1) {
     img1.style = 'display: block;';
-    s1.style = 'background: #E5E8EF; color: #DF2771;';
+    s1.style = 'background: #E5E8EF; color: #030748;';
   }
   if(n==2) {
     img2.style = 'display: block;';
-    s2.style = 'background: #E5E8EF; color: #DF2771;';
+    s2.style = 'background: #E5E8EF; color: #030748;';
   }
   if(n==3) {
     img3.style = 'display: block;';
-    s3.style = 'background: #E5E8EF; color: #DF2771;';
+    s3.style = 'background: #E5E8EF; color: #030748;';
   }
   if(n==4) {
     img4.style = 'display: block;';
-    s4.style = 'background: #E5E8EF; color: #DF2771;';
+    s4.style = 'background: #E5E8EF; color: #030748;';
   } 
 }
 
